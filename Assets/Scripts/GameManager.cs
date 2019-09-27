2f07f23fedea8ab4f15b3015109f0b26b7c50e49
donetello41 <demir_tl@hotmail.com>
2019-09-27 18:06:36 +0300

    Project Completed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public bool isGameStarted = false;
+    float elapsedTime = 3;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    private void Update()
+    {
+        elapsedTime -= Time.deltaTime;
+
+        if (elapsedTime > 0)
+            return;
+        else
+        {
+            StartGame();
+        }
+    }
+    private void StartGame()
+    {
+        isGameStarted = true;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
